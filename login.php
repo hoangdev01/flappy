041bd1eb4be501c80e13c5f384c5a2e5a8f26461
gasieuwaycuatk20 <67224126+gasieuwaycuatk20@users.noreply.github.com>
2021-10-10 21:57:00 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+//lấy dữ liệu từ login2.html
+$u = $_POST['username'];
+$p = $_POST['password'];
+
+//kết nối tới csdl
+$db = mysqli_connect("localhost","root","","web sun");
+
+//Truy vấn xem user và password có trong csdl không
+$sql = "select * from users where username='$u' and password='$p'";
+$rs = mysqli_query($db,$sql);
+if (mysqli_num_rows($rs)>0){
+    include 'menu.html';
+}else{
+    echo"<h2> Sai tai khoan hoac mat khau</h2>";
+}
+
+?>
